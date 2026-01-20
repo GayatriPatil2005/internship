9808a0f6fe1fc21de91584c35d5bd487659f32d6
GayatriPatil2005 <gmpatil9913@gmail.com>
2026-01-20 10:10:14 +0530

    APIs updated

--- APIs.js
+++ APIs.js
@@ -7,7 +7,20 @@ async function getData() {
     let list = document.querySelector(".list");
     data.forEach(user => {
         let entity = document.createElement("li");
-        entity.textContent = user.name;
+        entity.innerHTML =
+        `<Strong>id:</Strong>${user.id} <br/>
+        <Strong>name:</Strong>${user.name} <br/>
+        <Strong>email:</Strong>${user.email} <br/>
+        <Strong>street:</Strong>${user.address.street} <br/>
+        <Strong>city:</Strong>${user.address.city}<br/>
+        <Strong>lat:</Strong>${user.address.geo.lat}<br/>
+        <Strong>lng:</Strong>${user.address.geo.lng}<br/>
+        <Strong>zipcode:</Strong>${user.address.zipcode}<br/>
+        <Strong>suite:</Strong>${user.address.suite}<br/>
+        `
+        entity.style.background = "lightgray";
+        entity.style.height = "400px";
+        entity.style.width = "400px";
         list.appendChild(entity);
     });
 }
